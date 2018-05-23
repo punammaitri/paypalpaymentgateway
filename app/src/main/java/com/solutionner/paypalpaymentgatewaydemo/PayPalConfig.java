302f0bdd0dcee77c71f1b6a514a061b4369cd96e
punammaitri <punam.maitri1@gmail.com>
2018-05-23 11:39:02 +0530

    Test

---
+++ app/src/main/java/com/solutionner/paypalpaymentgatewaydemo/PayPalConfig.java
@@ -0,0 +1,6 @@
+package com.solutionner.paypalpaymentgatewaydemo;
+
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "ASgapNwgf4RUGyxgNrIb0azRZ4sCKSaqsgiOTzTIz-HHj4wJSo6LZfsbL5QRdz1vpTHgE2_rgcJdDR4u";
+}
